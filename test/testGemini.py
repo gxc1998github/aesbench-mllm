5e8b28fc3f38380c0b108f0503fa36ee7ff5b276
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2024-07-24 19:21:20 +1200

    modifying stuff

--- test/testGemini.py
+++ test/testGemini.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 
-apiKeyAI = "pip "
+apiKeyAI = "AIzaSyAFImNJvd1hkKTf7foNCBB0W-ouUG0UV5A"
 apiKeyGoogleCloud = "AIzaSyDetIFrg5lEACtNdJYeIYKbAZxseYseAuo" 
 
 #genai.configure(api_key=apiKeyAI)
@@ -11,7 +11,7 @@ genai.configure(api_key=apiKeyGoogleCloud)
 model = genai.GenerativeModel('gemini-1.5-pro')    
 
 
-response = model.generate_content("Write a story about an AI and magic, make it one sentence.")
+response = model.generate_content("Write a story about education, make it one sentence.")
 
 # Access the result attribute of the response
 result = response._result
@@ -19,4 +19,4 @@ result = response._result
 # Access the first candidate's content parts and text
 text_content = result.candidates[0].content.parts[0].text
 
-print(text_content)
+print("\n" + text_content)
