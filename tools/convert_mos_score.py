d2271343174e7a00c6cee92c95866931a81d59a2
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2024-08-31 02:52:23 +1200

    feat(matrix): write draft code for confusion matrix

--- tools/convert_mos_score.py
+++ tools/convert_mos_score.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the CSV file from the specified location
-mos_df = pd.read_csv('../data_release/mos_ground_truth.csv')
+mos_df = pd.read_csv('../data_release/original_ground_truth.csv')
 
 # Define the function to categorize MOS
 def categorize_mos(mos):
@@ -13,9 +13,9 @@ def categorize_mos(mos):
         return 'High'
 
 # Apply the transformations
-mos_df['AesA3_Score'] = (1 + 9 * mos_df['MOS']).round(4)  # Scale 1-10 with 4 decimal places
-mos_df['AesA2_Score'] = (1 + 4 * mos_df['MOS']).round(4)  # Scale 1-5 with 4 decimal places
-mos_df['AesA1_Score'] = mos_df['MOS'].apply(categorize_mos)  # Categorisation
+mos_df['AesA3_Score'] = (1 + 9 * mos_df['MOS']).round(0).astype(int)  # Scale 1-10
+mos_df['AesA2_Score'] = (1 + 4 * mos_df['MOS']).round(0).astype(int)  # Scale 1-5 
+mos_df['AesA1_Score'] = mos_df['MOS'].apply(categorize_mos)           # Categorisation
 
 # Save the updated dataframe to the specified location
-mos_df.to_csv('../data_release/ground_truth.csv', index=False)
+mos_df.to_csv('../data_release/new_ground_truth.csv', index=False)
