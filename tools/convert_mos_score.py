01e5804779b494c0bbd1b1e54af0c12e491f2aec
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2024-10-02 15:15:25 +1300

    refactoring codes

--- tools/convert_mos_score.py
+++ tools/convert_mos_score.py
@@ -1,3 +1,9 @@
+"""
+Description: This code is to create new ground truth from the orginal ground truth
+             by converting the high-medium-low to its equivalent numerical scores.
+             Original ground truth only have high-medium-low value for each image,
+             new ground truth will have both high-medium-low and numerical scores.
+"""
 import pandas as pd
 
 # Load the CSV file from the specified location
@@ -12,7 +18,7 @@ def categorize_mos(mos):
     else:
         return 'High'
 
-# Apply the transformations
+# Apply the transformations with relevant rules
 mos_df['AesA3_Score'] = (1 + 9 * mos_df['MOS']).round(0).astype(int)  # Scale 1-10
 mos_df['AesA2_Score'] = (1 + 4 * mos_df['MOS']).round(0).astype(int)  # Scale 1-5 
 mos_df['AesA1_Score'] = mos_df['MOS'].apply(categorize_mos)           # Categorisation
